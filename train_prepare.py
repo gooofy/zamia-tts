aafbdd51361a81dec9bc3f53f807f7710aa4a6f8
Guenter Bartsch <guenter@zamia.org>
2018-11-25 22:21:44 +0100

    add command line options

--- train_prepare.py
+++ train_prepare.py
@@ -42,16 +42,11 @@ from nltools           import misc
 from zamiatts          import DSFN_PATH, HPARAMS_SRC, DSFN_X, DSFN_XL, DSFN_YS, DSFN_YM, DSFN_YL, VOICE_PATH, cleanup_text
 from zamiatts          import audio
 
-# DEBUG_LIMIT  = 0
-DEBUG_LIMIT = 65
+DEBUG_LIMIT  = 0
+# DEBUG_LIMIT = 65
+# DEBUG_LIMIT = 512
 
 PROC_TITLE      = 'train_prepare'
-
-VOICE           = 'karlsson'
-GENDER          = 'male'
-MAILABSDIR      = '/home/bofh/projects/ai/data/speech/corpora/m_ailabs_de/de_DE/by_book/%s/%s' % (GENDER, VOICE)
-LANG            = 'de'
-
 TMP_WAV         = 'tmp/tmp.wav'
 
 def _decode_input(x):
@@ -76,7 +71,13 @@ misc.init_app (PROC_TITLE)
 # command line
 #
 
-parser = OptionParser("usage: %prog [options])")
+parser = OptionParser("usage: %prog [options] <voice_in> <voice_out>")
+
+parser.add_option ("-g", "--gender", dest="gender", type = "str", default="male",
+                   help="gender (default: male)")
+
+parser.add_option ("-l", "--lang", dest="lang", type = "str", default="de_DE",
+                   help="language (default: de_DE)")
 
 parser.add_option("-v", "--verbose", action="store_true", dest="verbose", 
                   help="enable debug output")
@@ -89,15 +90,24 @@ if options.verbose:
 else:
     logging.basicConfig(level=logging.INFO)
 
+if len(args) != 2:
+    parser.print_help()
+    sys.exit(0)
+
+voice_in   = args[0]
+voice_out  = args[1]
+lang       = options.lang.split('_')[0]
+mailabsdir = '/home/bofh/projects/ai/data/speech/corpora/m_ailabs_de/%s/by_book/%s/%s' % (options.lang, options.gender, voice_in)
+
 #
 # clean up / setup directory
 #
 
-cmd = 'rm -rf %s' % (DSFN_PATH % VOICE)
+cmd = 'rm -rf %s' % (DSFN_PATH % voice_out)
 logging.info(cmd)
 os.system(cmd)
 
-cmd = 'mkdir -p %s' % (DSFN_PATH % VOICE)
+cmd = 'mkdir -p %s' % (DSFN_PATH % voice_out)
 logging.info(cmd)
 os.system(cmd)
 
@@ -122,10 +132,10 @@ logging.info ('max_mfc_frames=%d, num_freq=%d, num_mels=%d' % (max_mfc_frames,hp
 training_data  = []
 num_skipped = 0
 
-for book in os.listdir(MAILABSDIR):
+for book in os.listdir(mailabsdir):
     logging.info('extracting training data from book %s' % book)
 
-    metafn = '%s/%s/metadata_mls.json' % (MAILABSDIR, book)
+    metafn = '%s/%s/metadata_mls.json' % (mailabsdir, book)
     if not os.path.exists(metafn):
         continue
 
@@ -138,7 +148,7 @@ for book in os.listdir(MAILABSDIR):
 
         ts_orig = meta[wavfn]['clean']
 
-        ts = cleanup_text(ts_orig, LANG, hparams['alphabet'])
+        ts = cleanup_text(ts_orig, lang, hparams['alphabet'])
 
         logging.debug(u'ts_orig %s' % ts_orig)
         logging.debug(u'ts      %s' % ts)
@@ -149,15 +159,22 @@ for book in os.listdir(MAILABSDIR):
             logging.error('%6d %-20s: transcript too long (%4d > %4d) %3d%% skipped' % (len(training_data), wavfn, len(ts), max_inp_len, pskipped))
             continue
 
-        wav_path = '%s/%s/wavs/%s' % (MAILABSDIR, book, wavfn)
+        wav_path = '%s/%s/wavs/%s' % (mailabsdir, book, wavfn)
 
         # cmd = 'sox %s %s silence -l 1 0.1 1%% -1 2.0 1%% compand 0.02,0.20 5:-60,-40,-10 -5 -90 0.1' % (wav_path, TMP_WAV)
-        cmd = 'sox %s %s silence 1 0.15 0.5%% reverse silence 1 0.15 0.5%% reverse gain -n -3' % (wav_path, TMP_WAV)
+        cmd = 'sox %s -r 16000 -b 16 -c 1 %s silence 1 0.15 0.5%% reverse silence 1 0.15 0.5%% reverse gain -n -3' % (wav_path, TMP_WAV)
         logging.debug(cmd)
         os.system(cmd)
 
         wav = audio.load_wav(TMP_WAV)
         # FIXME: remove, we're using sox for this now wav = audio.trim_silence(wav, hparams)
+
+        # logging.debug('wav: %s' % wav.shape)
+        if wav.shape[0] < 512:
+            num_skipped += 1
+            pskipped = num_skipped * 100 / (len(training_data) + num_skipped)
+            logging.error('%6d %-20s: audio too short (%4d < 512) %3d%% skipped' % (len(training_data), wavfn, len(ts), pskipped))
+            continue
  
         spectrogram     = audio.spectrogram(wav, hparams).astype(np.float32)
         mel_spectrogram = audio.melspectrogram(wav, hparams).astype(np.float32)
@@ -225,18 +242,18 @@ for i, (ts, S, M) in enumerate(training_data):
 
     if batch_idx == (batch_size-1):
 
-        np.save(DSFN_X % (VOICE, batch_num), input_data)
-        logging.info("%s written. %s" % (DSFN_X % (VOICE, batch_num), input_data.shape))
+        np.save(DSFN_X % (voice_out, batch_num), input_data)
+        logging.info("%s written. %s" % (DSFN_X % (voice_out, batch_num), input_data.shape))
 
-        np.save(DSFN_XL % (VOICE, batch_num), input_lengths)
-        logging.info("%s written. %s" % (DSFN_XL % (VOICE, batch_num), input_lengths.shape))
+        np.save(DSFN_XL % (voice_out, batch_num), input_lengths)
+        logging.info("%s written. %s" % (DSFN_XL % (voice_out, batch_num), input_lengths.shape))
 
-        np.save(DSFN_YS % (VOICE, batch_num), target_data_s)
-        logging.info("%s written. %s" % (DSFN_YS % (VOICE, batch_num), target_data_s.shape))
+        np.save(DSFN_YS % (voice_out, batch_num), target_data_s)
+        logging.info("%s written. %s" % (DSFN_YS % (voice_out, batch_num), target_data_s.shape))
 
-        np.save(DSFN_YM % (VOICE, batch_num), target_data_m)
-        logging.info("%s written. %s" % (DSFN_YM % (VOICE, batch_num), target_data_m.shape))
+        np.save(DSFN_YM % (voice_out, batch_num), target_data_m)
+        logging.info("%s written. %s" % (DSFN_YM % (voice_out, batch_num), target_data_m.shape))
 
-        np.save(DSFN_YL % (VOICE, batch_num), target_lengths)
-        logging.info("%s written. %s" % (DSFN_YL % (VOICE, batch_num), target_lengths.shape))
+        np.save(DSFN_YL % (voice_out, batch_num), target_lengths)
+        logging.info("%s written. %s" % (DSFN_YL % (voice_out, batch_num), target_lengths.shape))
 
